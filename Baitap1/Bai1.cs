4d486ca0b6799f0befca11daa0a607529df3634d
Internetexploree <lequi1322@gmail.com>
2023-03-24 00:28:22 +0700

    Add project files.

---
+++ Baitap1/Bai1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baitap1
+{
+    internal class Bai1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chao mung ban den voi ung dung cua Console application");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("Nhap phim bat ky de ket thuc chuong trinh");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Baitap2/Bai2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baitap2
+{
+    internal class Bai2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Moi nhap ho va ten: ");
+            string hoTen = Console.ReadLine();
+
+            Console.Write("Moi nhap gioi tinh: ");
+            string gioiTinh = Console.ReadLine();
+
+            Console.Write("Moi nhap so tuoi: ");
+            int soTuoi = Int16.Parse(Console.ReadLine());
+
+            Console.WriteLine("Cac thong tin ma ban vua nhap nhu sau:");
+            Console.WriteLine("Ho ten cua ban la:\t {0}", hoTen);
+            Console.WriteLine("Gioi tinh cua ban la:\t {0}", gioiTinh);
+            Console.WriteLine("So tuoi cua ban la:\t {0}", soTuoi);
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Bam phim bat ky de ket thuc!");
+            Console.ReadKey();
+        }
+    }
+}
